9d9c6679069daaa161467e94551bdec516d5b6fb
SWIFTAIR-COMPANY <kerrisksiun@gmail.com>
2025-05-22 18:46:57 +0100

    Update with latest changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,5 @@
-import React from "react";
-import logo from "../assets/swiftair-logo.png";
+/* Header.jsx */ import React from "react"; import logo from "../assets/swiftair-logo.png";
 
-const Header = ({ isTranslated, toggleTranslation }) => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img
-          src={logo}
-          alt="SwiftAir Logo"
-          className="logo"
-          onClick={() => (window.location.href = "/")}
-        />
-      </div>
-      <button className="language-toggle" onClick={toggleTranslation}>
-        {isTranslated ? "Switch to English" : "切换到中文"}
-      </button>
-    </div>
-  );
-};
+const Header = ({ isTranslated, toggleTranslation }) => { return ( <div className="header"> <div className="logo-container"> <img src={logo} alt="SwiftAir Logo" className="logo" onClick={() => (window.location.href = "/")} /> </div> <div className="language-toggle-container"> <button className="language-toggle" onClick={toggleTranslation}> {isTranslated ? "Switch to English" : "切换到中文"} </button> </div> </div> ); };
 
 export default Header;
